2df4e22d5ea92df86099e3b48ca141662af77c48
Nikhil <nikhil956839@gmail.com>
2021-12-30 15:50:30 +0530

    First Commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+     public static void main(String[] args){
+          System.out.println("Hello Nikhil!!");
+          int myFirstNum = 5;
+          int mySecondNum = 12;
+          int myThirdNum = 6;
+          int myTotal = myFirstNum + mySecondNum + myThirdNum;
+          int myLastOne = 1000 - myTotal;
+          System.out.println(myLastOne);
+
+     }
+}
