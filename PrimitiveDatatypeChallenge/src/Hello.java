47b14d6dbbeefba076552968b929dfb6a3436892
Nikhil <nikhil956839@gmail.com>
2022-01-01 17:33:00 +0530

    Primitive Challenges

---
+++ PrimitiveDatatypeChallenge/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+    public static void main(String[] args)
+    {
+        byte myChallengeByte = 10;
+        short myChallengeShort = 20;
+        int myChallengeInt = 50;
+
+        int myChallengeSum = myChallengeByte + myChallengeInt + myChallengeShort;
+
+        long myChallengeLong = 50000L + 10L*(myChallengeSum);
+
+        System.out.println("Long Challenge Value: "+ myChallengeLong);
+
+//        System.out.println(myChallengeLong.getClass().getSimpleName());S
+    }
+}
